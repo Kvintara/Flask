979db6e21c769024fc5814bf6b93441d472f4809
Kvintara <Kvintara@gmail.com>
2023-03-03 15:48:37 +0200

    sestas

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-def index(title):
-    return render_template("index.html", title=title)
+def index():
+    return render_template("index.html")
 
 
 @app.route("/game<string:title>")
